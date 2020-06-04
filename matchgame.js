f669ba27372c68538a9ce0c5bd29d58e1cb9bdb8
Odaine Codner <odaine.codner@gmail.com>
2020-06-04 10:49:52 +0100

    flip?

--- matchgame.js
+++ matchgame.js
@@ -5,6 +5,7 @@ let firstCard, secondCard;
 
 function turnCardOver() {
     console.log("Card has been clicked");
+    this.classList.toggle('flip');
 }
 
 cards.forEach(card => addEventListener('click', turnCardOver));
