169c08f6ff4622925ddc4b232217ef41fb0b5eb8
Odaine Codner <odaine.codner@gmail.com>
2020-06-04 13:54:53 +0100

    match

--- matchgame.js
+++ matchgame.js
@@ -5,16 +5,27 @@ let firstCard, secondCard;
 
 function turnCardOver() {
     console.log("Card has been clicked");
-    this.classList.add('flip');
-    
+    this.classList.add('match');
+
+    //First  card clicked
 if (!cardHasTurned) {
     cardHasTurned = true;
     firstCard = this;
 } else {
+    //Second card clicked
     cardHasTurned = false;
     secondCard = this;
+    
+    //Check for matches
+    if(firstCard.dataset.name === secondCard.dataset.name) {
+        firstCard.removeEventListener('click', turnCardOver);
+        secondCard.removeEventListener('click', turnCardOver);
+        console.log("match found")
+     } else {
+         firstCard.classList.remove('match')
+     }
 }
 
-}
+
 
 cards.forEach(card => addEventListener('click', turnCardOver));
